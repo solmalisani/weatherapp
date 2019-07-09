f4ad67e8ecc37b33b62468ee8f873606b7bf5b3b
solmalisani <solmalisani@gmail.com>
2019-07-08 21:25:27 -0300

    cambio url

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { PropTypes } from 'prop-types';
+import getUrlWeatherByCity from './../../services/getUrlWeatherByCity';
 import transformWeather from './../../services/transformWeather';
-import { api_weather } from './../../constants/api_url';
 import Location from './Location';
 import WeatherData from './WeatherData';
 
 class WeatherLocation extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    const { city } = props;
+
+    super(props);
     this.state = {
-      city: "Buenos Aires",
+      city,
       data: null,
     }
     console.log("constructor");
@@ -24,6 +28,7 @@ class WeatherLocation extends Component {
   }
     
   handleUpdateClick = () => {
+    const api_weather = getUrlWeatherByCity(this.state.city);
     fetch(api_weather).then ( resolve => {
       
         return resolve.json();
@@ -45,11 +50,15 @@ class WeatherLocation extends Component {
         <Location city={city}> </Location>
         {data ? 
         <WeatherData data={data}></WeatherData> :
-        "Cargando..."
+        < CircularProgress size = {50}>  </ CircularProgress >
         }
       </div>
     );
   }
+}
+
+WeatherLocation.protoTypes = {
+  city: PropTypes.string.isRequired,
 }
 
 export default WeatherLocation;
--- src/constants/api_url.js
+++ src/constants/api_url.js
@@ -1,7 +1,2 @@
-
-
-const location = "Buenos Aires,ar"
-const api_key = "d123f8be74c0dba159dcb76c29c8b93d"
-const url_base_weather = "http://api.openweathermap.org/data/2.5/weather"
-
-export const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`
+export const api_key = "d123f8be74c0dba159dcb76c29c8b93d"
+export const url_base_weather = "http://api.openweathermap.org/data/2.5/weather"
